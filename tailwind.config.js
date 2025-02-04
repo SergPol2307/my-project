fe2ddb606cd7d9e3b84ee1eb6f2c625a243dcdac
SergPol <sergpol2307@gmail.com>
2025-02-04 18:29:53 -0500

    first version of project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content:["./src/**/*.{html,js,ts,jsx,tsx}"],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                mainColor: '#050C2A'
+            },
+            padding: {
+                big: '50px'
+            }
+        },
+    },
+    plugins: [],
+};
